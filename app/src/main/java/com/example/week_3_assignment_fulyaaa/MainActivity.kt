c1e06e87890438ff7fdbb283afd6f9e02e2c7d2d
fakan <fulyaakan@outlook.com>
2022-10-09 03:59:48 +0300

    Fragments layout + First and second fragment binding

--- app/src/main/java/com/example/week_3_assignment_fulyaaa/MainActivity.kt
+++ app/src/main/java/com/example/week_3_assignment_fulyaaa/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.week_3_assignment_fulyaaa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvProductList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavController()
+    }
+    private fun setupNavController(){
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
     }
 }
+
+
